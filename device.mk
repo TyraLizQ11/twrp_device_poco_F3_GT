5a19f4d50143869b10e9a2ad557862707a000ded
donsenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-09-25 23:07:20 +0800

    aresin: delete not needed

--- device.mk
+++ device.mk
@@ -72,7 +72,6 @@ TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.0/file"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
 TW_Y_OFFSET := 80
