198d11ae115ed1e5579ef5a68ba71260d828962e
donsenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-10-02 00:17:25 +0800

    aresin: add for decryption

--- device.mk
+++ device.mk
@@ -104,3 +104,15 @@ CUSTOM_IMAGE_VERITY_BLOCK_DEVICE := /dev/block/bootdevice/by-name/system_ext
 # Installer
 USE_RECOVERY_INSTALLER := true
 RECOVERY_INSTALLER_PATH := $(LOCAL_PATH)/installer
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
